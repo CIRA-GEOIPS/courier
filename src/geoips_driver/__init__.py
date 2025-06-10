fe65be0d22105b4ae55a41d18f4b0b98fd3234a3
BSL-5 <biosafetylevel5@gmail.com>
2025-06-10 22:45:28 +0000

    Move driver code to src dir

--- src/geoips_driver/__init__.py
+++ src/geoips_driver/__init__.py
@@ -1,16 +1,20 @@
-import logging
+# # # Distribution Statement A. Approved for public release. Distribution unlimited.
+# # #
+# # # Author:
+# # # Naval Research Laboratory, Marine Meteorology Division
+# # #
+# # # This program is free software: you can redistribute it and/or modify it under
+# # # the terms of the NRLMMD License included with this program. This program is
+# # # distributed WITHOUT ANY WARRANTY; without even the implied warranty of
+# # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the included license
+# # # for more details. If you did not receive the license, for more information see:
+# # # https://github.com/U-S-NRL-Marine-Meteorology-Division/
 
-try:
-    import importlib.metadata
+"""Template repository demonstrating a basic GeoIPS plugin example."""
 
-    __version__ = importlib.metadata.version(__package__ or __name__)
-except ModuleNotFoundError:
-    try:
-        import importlib_metadata
+# NOTE: _version.py is generated automatically during build/install
+from ._version import __version__, __version_tuple__
 
-        __version__ = importlib_metadata.version(__package__ or __name__)
-    except ModuleNotFoundError:
-        logging.debug(
-            "Could not set __version__ because importlib.metadata is not available."
-            + "If running python 3.7, installing importlib-metadata will fix this issue"
-        )
+from geoips_driver import interfaces
+
+__all__ = ["__version__", "__version_tuple__", "interfaces"]
